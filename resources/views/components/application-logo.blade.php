ab3aaa8bb6bed6f161279e9f5567c497b4cc9ba6
hsharan-stha <hsharan.shrestha@gmnail.com>
2025-06-09 09:05:21 +0900

    initialize

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+     viewBox="0 0 24 24" width="48" height="48">
+  <path d="M4 19.5A2.5 2.5 0 016.5 17H20"/>
+  <path d="M4 4.5A2.5 2.5 0 016.5 7H20v13H6.5A2.5 2.5 0 014 17.5v-13z"/>
+  <path d="M20 7l-8 5-8-5"/>
+</svg>
