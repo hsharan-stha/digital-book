959b5ea83bfae7e3844fe4bc80991d23993c9abe
hsharan-stha <hsharan.shrestha@gmnail.com>
2025-06-11 17:34:35 +0900

    fix

--- resources/views/layouts/entry.blade.php
+++ resources/views/layouts/entry.blade.php
@@ -26,13 +26,15 @@
         <div
             class="flex items-center justify-between px-7 py-3 sticky top-0 border-b bg-gray-900 text-gray-200 z-[1111] h-[64px]">
             <div class="flex items-center gap-4">
+                  <a href="/" class="flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-6 text-gray-200">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                 </svg>
+                                <span class="text-xl tracking-wide text-gray-200 xs:hidden">Digital Book</span>
 
-                <span class="text-xl tracking-wide text-gray-200 xs:hidden">Digital Book</span>
+</a>
             </div>
 
             <div class="flex items-center space-x-4">
@@ -47,14 +49,14 @@
                     </svg>
 
                 </div>
-                <a href="#" class="flex items-center gap-2">
+                <a href="#" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                 </a>
-                <a href="/library" class="flex items-center gap-2">
+                <a href="/library" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
