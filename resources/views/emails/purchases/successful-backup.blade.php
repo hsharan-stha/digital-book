1aafc5884b876c809a2058c90edfbf99197b61e7
hsharan-stha <hsharan.shrestha@gmail.com>
2025-08-21 09:15:46 +0900

    reader page

--- resources/views/emails/purchases/successful-backup.blade.php
+++ resources/views/emails/purchases/successful-backup.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-# 専門教育出版デジタルブックの詳細
+# 専門教育出版デジタルブック振込先の確認
 
 {{ $purchase->user->name ?? 'Customer' }} 様
 
@@ -16,6 +16,25 @@
 | {{ $detail->book->name ?? 'Unknown' }} | {{ $detail->quantity }} | ¥{{ number_format($detail->price) }} |
 @endforeach
 @endcomponent
+
+---
+
+**以下の口座に金額をお振込みください**
+
+
+@component('mail::table')
+| 口座情報          |              |
+| --------------- | ----------------------- |
+| 銀行名          | 千葉信用金庫            |
+| 支店名          | 大佐和支店              |
+| 種類            | 普通                    |
+| 口座番号        | XXXXXXX7897             |
+| 名前            | ｾﾝﾓﾝｷｮｳｲｸｼｭｯﾊﾟﾝ         |
+@endcomponent
+
+---
+
+以上。よろしくお願いいたします。
 
 よろしくお願いいたします,  
 {{ config('app.name') }}
